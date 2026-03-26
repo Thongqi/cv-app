0ed9cc77553a0711c2fbbd970ddd9e09f3d65327
Thongqi <141812298+Thongqi@users.noreply.github.com>
2026-03-26 10:22:07 +0800

    add in handle input change

--- cv-app/src/components/input.jsx
+++ cv-app/src/components/input.jsx
@@ -1,8 +1,31 @@
-export function Input(labelname) {
+import { useState } from "react";
+
+function Input(labelname, value) {
   return (
     <>
       <label>{labelname}</label>
-      <input name="name" placeholder="" />
+      <input name="name" value={value} onChange={handleChange}/>
     </>
   );
 }
+
+function handleChange(event){
+  setValue(event.target.value)
+}
+
+function changeHTML(labelname, value){
+  return(
+    <div>
+      {value}
+    </div>
+  )
+}
+
+export function handleInput(labelname, value){
+  const [value, setValue] = useState("")
+
+  return(
+    <Input labelname={labelname} value={value}></Input>
+  )
+}
+
